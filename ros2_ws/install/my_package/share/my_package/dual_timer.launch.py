92ffb8e7b61a0a2d154852665d830719d23716b5
useok <useok2791@gmail.com>
2025-04-22 21:20:56 +0900

    WIP 020304 Topic Programming - subscriber

--- ros2_ws/install/my_package/share/my_package/dual_timer.launch.py
+++ ros2_ws/install/my_package/share/my_package/dual_timer.launch.py
@@ -1,16 +1,1 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='my_package',
-            executable='timer_node',
-            name='my_timer_node'
-        ),
-        Node(
-            package='my_package',
-            executable='fast_timer_node',
-            name='my_fast_timer_node',
-        )
-    ])
+/root/ros2_ws/build/my_package/launch/dual_timer.launch.py
---
+++ ros2_ws/src/tutorial_topic/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'tutorial_topic'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='useok2791@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'move_publisher = tutorial_topic.move_publisher:main'
+        ],
+    },
+)
